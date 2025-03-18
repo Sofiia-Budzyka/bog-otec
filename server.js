013d61e63b2600dd3b698865987979a9985c5e9b
Sofiia <sofiya2007budzuka@gmail.com>
2025-03-18 22:10:45 +0200

    .

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/index.html', (_, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/plans.html', (_, res) => {
+    res.sendFile(__dirname + '/plans.html');
+});
+
+app.listen(1488);
